f3dc6b6893e6fa73ef2decc76f627c53223da9ef
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-05 15:28:07 -0400

    Implemented tweet splitter
    Naive and ideal splitter methods implemented, although both are less
    than ideal in their implementation. Lots of repetition, but fortunately
    tests are in place so refactoring can move forward.

--- lib/long_tweet.rb
+++ lib/long_tweet.rb
@@ -1,5 +1,62 @@
 require "long_tweet/version"
 
 module LongTweet
-  # Your code goes here...
+  class Tweet
+    attr_reader :text
+    def initialize text
+      @text = text
+    end
+
+    def send
+      "Sending Tweet: #{text}"
+    end
+
+  end
+
+  class Splitter
+    attr_reader :text, :tweet_count
+    def initialize text
+      @text = text
+      set_tweet_count
+    end
+
+    def set_tweet_count
+      @tweet_count = text.length/140
+      @tweet_count += 1 if text.length.modulo(140) > 0
+    end
+
+    def split
+      ary = text.split
+      if ary.any? {|e| e.length > 140 }
+        naive_split
+      else
+        normal_split
+      end
+    end
+
+    def naive_split
+      a = []
+      i = 0
+      tweet_count.times do
+        a << text.slice(i*140,140)
+        i += 1
+      end
+      a
+    end
+
+    def normal_split
+      a = []
+      ary = text.split
+      tweet = ''
+      while ary.length > 0
+        while ary.length > 0 && (tweet.length + ary.shift.length) < 140
+          tweet << " #{ary.shift}"
+        end
+        a << tweet
+        tweet = ''
+      end
+      a
+    end
+
+  end
 end
---
+++ spec/long_tweet_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe LongTweet::Tweet do
+  let(:text) { 'some sample text to tweet' }
+
+  it 'should accept text' do
+    LongTweet::Tweet.new(text)
+  end
+
+  it 'should notify the user of sending a tweet' do
+    result = LongTweet::Tweet.new(text).send
+    expect(result).to eq "Sending Tweet: #{text}"
+  end
+end
+
+describe LongTweet::Splitter do
+  subject { LongTweet::Splitter }
+  let(:text) { 'a'*140*3 }
+  let(:lorem) { %w{lorenm ipsummm dolor sit ament} }
+  let(:words) { (lorem*8).join(' ') }
+
+  it 'should create a tweet for every 140 characters' do
+    tweets = subject.new(text).split
+    expect(tweets.first.length).to eq 140
+  end
+
+  it 'should know its tweet count' do
+    expect(subject.new(text).tweet_count).to eq 3
+  end
+
+  it 'only have full words in tweets' do
+    first_tweet = subject.new(words).split.first
+    last_word = first_tweet.split.last
+    expect(lorem.include? last_word).to be_true
+  end
+
+end
